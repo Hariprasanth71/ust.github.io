de50b807a6172024f9e4ff10f095f2da12e47b48
Hariprasanth <hariprasanth@piktorlabs.com>
2022-08-28 20:22:56 +0530

    pro

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let slideIndex = 1;
 show(slideIndex);
 
-function Pl_slide(n) {
-  show(slideIndex += n);
+function Pl_slide(n)
+{
+  show(slideIndex+=n);
 }
 
 function show(n){
@@ -14,6 +15,7 @@ function show(n){
   {
     document.getElementById("Preva").style.display="none";
   }
+
   else
   {
     document.getElementById("Preva").style.display="block";
@@ -36,7 +38,6 @@ function show(n){
 
 
 const menu = document.querySelector(".menu");
-// const menuItems = document.querySelector(".menuItems");
 const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
@@ -54,33 +55,12 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener("click", toggleMenu);
-
 menuItems.forEach( 
-  function(menuItem) { 
+  function(menuItem)
+  { 
     menuItem.addEventListener("click", toggleMenu);
   }
 )
-
-
-
-function serch() {
-  var input, filter, ta, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ta = document.getElementById("main-contents");
-  tr = document.getElementById("contents");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementById("do");
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
-  }
-}
 
 
 function arrows(direction){
@@ -99,139 +79,20 @@ function arrows(direction){
       }
   }, 17);
 }
-// function showSlidess(d){
-//   let i;
-//   let sli= document.getElementById("mo_scroll");
-//   if (d > sli.length) {slide_ = 1}    
-//   if (d < 1) {slide_ = sli.length}
-//   for (i = 0; i < sli.length; i++) 
-//   {
-//     sli[i].style.display = "none";
-    
-//   }
-//   sli[slide_ - 1].style.display = "flex";  
- 
-// }
-// let slide_= 1;
-// showSlidess(slide_);
 
-// function arrows(d) {
-//   showSlidess(slide_ += d);
-// }
-
-// let count = 0;
-//  const cards=document.getElementById('act_bar_1');
-// const len=cards.length;
-
-// function scrollFunction1(){
-//   let i;
-//   let cards = document.getElementsByClassName("act_bar_1");
-//   for (i = 0; i < cards.length-4; i++) {
-//      document.getElementById("act_bar").remove();
-//   }
-// }
-// const slide=document.querySelectorAll(".container-item");
-// const slidecontainer=document.querySelector(".stat-container");
-
-// const nextln=document.querySelector(".btn");
-// let slidewidth=slide[0].clientWidth;
-// let currentslide=0;
-
-// nextln.addEventListener("click",()=>{
-//   if(currentslide>=numberofImages-1)
-//   {
-//     gotoslide(0);
-//     currentslide=0;
-//     return;
-//   }
-//   currentslide++;
-//   gotoslide(currentslide);
-// });
-
-// function gotoslide(slidenumber)
-// {
-//   slidecontainer.style.transform="translateX(-" + slidewidth *slidenumber + "px)";
-// }
-
-
-
-// function next()
-// {
-//   document.getElementById("arr--next").classList.add("change");
-// }
-// const slides= document.querySelector(".container-item");
-// slides.forEach((container-item,indx) => {
-//   container-item.style.transform=`translateX(${index*100}%)`;
-// });
-
-
-
-
-
-
-
-
-
-// v// drop down the menu, and swap the icon to the close icon
-// $('.menu').click(function(){
-//   $(this).toggleClass('icon-menu');
-//   $(this).toggleClass('icon-cross');
-//   $('nav').toggleClass('down');
-//   $('nav li a').removeClass('down');
-//   $('.search').removeClass('down');
-//   $('.icon-search').removeClass('icon-cross');
-// });
-
-// //Make sure the menu icon behaves corectly when the menu is open
-// $('nav li a').click(function(){
-//     $('.menu').addClass('icon-menu');
-//     $('.menu').removeClass('icon-cross');
-//     $('nav').toggleClass('down');
-// });
-
-// //show and hide the search bar, also make sure if the menu is open to hide the menu, and ensure the menu icon state is correct
-// $('.icon-search').click(function(){
-//     $(this).toggleClass('icon-cross');
-//     $('.menu').addClass('icon-menu');
-//     $('.menu').removeClass('icon-cross');
-//     $('.search').toggleClass('down');
-//     $('nav').removeClass('down');
-// });
-// let slidePosition = 0;
-// const slides = document.getElementsByClassName('container-item');
-// const totalSlides = slides.length;
-
-// document.
-//   getElementById('arr--next')
-//   .addEventListener("click", function () {
-//     moveToNextSlide();
-//   });
-
-// function updateSlidePosition() {
-//   for (let slide of slides) {
-//     slide.classList.remove('container-item--visible');
-//     slide.classList.add('container-item--hidden');
-//   }
-
-//   slides[slidePosition].classList.add('container-item--visible');
-// }
-
-// function moveToNextSlide() {
-//   if (slidePosition === totalSlides - 1) {
-//     slidePosition = 0;
-//   } else {
-//     slidePosition++;
-//   }
-
-//   updateSlidePosition();
-// }
-
-// function moveToPrevSlide() {
-//   if (slidePosition === 0) {
-//     slidePosition = totalSlides - 1;
-//   } else {
-//     slidePosition--;
-//   }
-
-//   updateSlidePosition();
-// }
+function serch() {
+  let input = document.getElementById('myInput').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('content');
+  
+  for (i = 0; i < x.length; i++) 
+  {
+      if (!x[i].innerHTML.toLowerCase().includes(input)) 
+      {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";             
+      }
+  }
+}
